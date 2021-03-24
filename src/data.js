14d0e99953b2d66ed92b8e53add559db03e3f183
humzahash01 <76885175+humzahash01@users.noreply.github.com>
2021-03-24 17:36:45 -0400

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,77 @@
+/*Staff members*/
+export const people = [
+  {
+    name: 'Humza Hashmi',
+    position: 'Coordinator',
+    email: 'hhashmi4@uwo.ca',
+    about:
+      "Humza is a software engineer who likes to code!!!",
+    
+  },
+  {
+    name: 'Angelina',
+    position: 'Coordinator ',
+    email: 'alapadul@uwo.ca',
+    about:
+      'Angelina loves to dance',
+    
+  },
+  {
+    name: 'Sarah Massad',
+    position: 'Coordinator',
+    email: 'smassad2@uwo.ca',
+    about:
+      'Sarah drove a car',
+    
+  },
+  {
+    name: 'Qali Langstaff',
+    position: 'Coordinator',
+    email: 'unknown',
+    about:
+      'just chilling and vibing',
+    
+  },
+  {
+    name: 'Adam Seif',
+    position: 'Coordinator',
+    email: 'aseif4@uwo.ca',
+    about:
+      'adam likes to ride his bike',
+    
+  }
+
+
+  
+];
+
+/*Articles on the home page*/
+export const homeArticles = [
+  {
+    title: 'Asynchronous',
+    content:
+      'data.',
+    img: {
+      name: 'stethoscope.jpg',
+      alt: 'stethoscope'
+    }
+  },
+  {
+    title: 'Security',
+    content:
+      'data.',
+    img: {
+      name: 'medic.jpg',
+      alt: 'medic'
+    }
+  },
+  {
+    title: "Lip Reader",
+    content:
+      'data.',
+    img: {
+      name: 'dna.jpg',
+      alt: 'dna'
+    }
+  }
+];
